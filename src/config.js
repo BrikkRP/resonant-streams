49c9229a5047e2f86e4690e0caf5139aee23338a
BrikkRP <brikktwitch@gmail.com>
2026-06-12 18:18:30 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,8 +4,13 @@ module.exports = {
     token: process.env.DISCORD_TOKEN,
     clientId: process.env.CLIENT_ID,
     guildId: process.env.GUILD_ID,
+
     liveRoleId: process.env.LIVE_ROLE_ID,
     logChannelId: process.env.LOG_CHANNEL_ID,
+
+    liveAnnounceChannelId: process.env.LIVE_ANNOUNCE_CHANNEL_ID,
+    livePingRoleId: process.env.LIVE_PING_ROLE_ID,
+
     keywords: (process.env.KEYWORDS || '')
         .split(',')
         .map(keyword => keyword.trim().toLowerCase())
